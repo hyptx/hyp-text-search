e2d267e3dba27a4cc76f96a62b556adcc111a862
adam <adam@510e8c62-e178-493f-8e20-3d6ef38f8346>
2013-08-05 16:31:59 +0000

    first commit

---
+++ hyp-text-search.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Hyp Text Search
+Plugin URI: http://textsearch.myhyperspace.com
+Description: A client side text search plugin
+Version: 1.0
+Author: Adam J Nowak
+Author URI: http://hyperspatial.com
+License: GPL2
+*/
+
+define('HYPTS_PLUGIN',WP_PLUGIN_URL . '/' . basename(dirname(__FILE__)) . '/');
+define('HYPTS_PLUGIN_SERVERPATH',dirname(__FILE__) . '/');
+
+
+?>
